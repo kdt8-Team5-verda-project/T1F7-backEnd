2cda2e87d577c369af2fb81041fcca8f1dec53cb
kimjonghyeon6084 <ssoert123@naver.com>
2023-11-18 11:31:44 +0900

    feat: login & fund Repository & entity 수정 #1

--- BE/src/main/java/com/verda/BE/login/repository/FundRepository.java
+++ BE/src/main/java/com/verda/BE/login/repository/FundRepository.java
@@ -1,9 +1,9 @@
 package com.verda.BE.login.repository;
 
-import com.verda.BE.login.entity.UserEntity;
+import com.verda.BE.login.entity.FundEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface KakaoRepository extends JpaRepository<UserEntity, Long> {
+public interface FundRepository extends JpaRepository<FundEntity, Long> {
 }
