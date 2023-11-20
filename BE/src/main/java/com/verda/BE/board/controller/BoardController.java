cae171bab84167b192d6131d6bd11f43dda8f7b5
kimjonghyeon6084 <ssoert123@naver.com>
2023-11-20 10:13:32 +0900

    fix: 충돌수정 #1

--- BE/src/main/java/com/verda/BE/board/controller/BoardController.java
+++ BE/src/main/java/com/verda/BE/board/controller/BoardController.java
@@ -1,12 +1,11 @@
 package com.verda.BE.board.controller;
 
-
-import com.verda.BE.board.service.BoardSerivce;
+import com.verda.BE.board.service.BoardService;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class BoardController {
-    private BoardSerivce boardSerivce;
+    private BoardService boardSerivce;
 
 //    @PostMapping("/api/v1/boards")
 //    public Long save(@RequestBody BoardRequestDTO requestDTO){
