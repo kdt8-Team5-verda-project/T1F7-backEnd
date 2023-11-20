ed5789b192555183bf40b2588ba02340ad83f5f5
saebyeok1 <qhdid3734@gmail.com>
2023-11-20 10:30:26 +0900

    fix : board api 경로 수정, domain 삭제 #2

--- BE/src/main/java/com/verda/BE/board/controller/BoardController.java
+++ BE/src/main/java/com/verda/BE/board/controller/BoardController.java
@@ -6,6 +6,7 @@ import com.verda.BE.board.dto.requestdto.BoardUpdateRequestDTO;
 import com.verda.BE.board.dto.responsedto.BoardListResponseDTO;
 import com.verda.BE.board.dto.responsedto.BoardResponseDTO;
 import com.verda.BE.board.service.BoardService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,13 +20,12 @@ public class BoardController {
     @Autowired
     private BoardService boardService;
 
-//
-//    @ApiResponses({
-//            @ApiResponse(responseCode = "200", description = "OK !!"),
-//            @ApiResponse(responseCode = "400", description = "BAD REQUEST !!"),
-//            @ApiResponse(responseCode = "404", description = "NOT FOUND !!"),
-//            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR !!")
-//    })
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK !!"),
+            @ApiResponse(responseCode = "400", description = "BAD REQUEST !!"),
+            @ApiResponse(responseCode = "404", description = "NOT FOUND !!"),
+            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR !!")
+    })
 
 
 //    @PostMapping("/board/write")
@@ -55,42 +55,40 @@ public class BoardController {
 //                .build();
 //    }
 
-//    @PostMapping("/board")
-//    @ResponseBody
-//    public void postBoard(@RequestBody BoardRequestDTO requestDTO) {
-//        boardService.createPost(requestDTO);
-//    }
 
     //    게시물 작성
-    @PostMapping("/board")
-//    왜 이거 넣으면 오류가??...
-//    @Operation(summary = "게시물 생성", description = "게시물을 작성합니다.")
+    @PostMapping("/api/board")
+    @Operation(summary = "게시물 생성", description = "게시물을 작성합니다.")
     public Long createPost(@RequestBody BoardCreateRequestDTO requestDto) {
         return boardService.create(requestDto);
     }
 
     //    게시물 수정
-    @PutMapping("/board/{postId}")
+    @PutMapping("/api/board/{postId}")
+    @Operation(summary = "게시물 수정", description = "게시물을 수정합니다.")
     public Long updatePost(@PathVariable Long postId, @RequestBody BoardUpdateRequestDTO requestDto) {
         return boardService.update(postId, requestDto);
     }
 
     //    게시물 조회
     //      개별 조회
-    @GetMapping("/board/{postId}")
+    @GetMapping("/api/board/{postId}")
+    @Operation(summary = "게시물 개별 조회", description = "게시물을 개별로 조회합니다.")
     public BoardResponseDTO searchById(@PathVariable Long postId) {
         return boardService.searchById(postId);
     }
 
     //    전체 조회(목록)
-    @GetMapping("/board")
+    @GetMapping("/api/board")
+    @Operation(summary = "게시물 전체 조회", description = "게시물들을 조회합니다.")
     public List<BoardListResponseDTO> searchAllDesc() {
         return boardService.searchAllDesc();
     }
 
     //    게시물 삭제
-    @DeleteMapping("/board/{id}")
-    public void delete(@PathVariable Long postId) {
+    @DeleteMapping("/api/board/{postId}")
+    @Operation(summary = "게시물 삭제", description = "게시물을 삭제합니다.")
+    public void deletePost(@PathVariable Long postId) {
         boardService.delete(postId);
     }
 }
