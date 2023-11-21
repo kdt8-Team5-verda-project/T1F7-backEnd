61a59d83fed30047aa6d7e3a3943e0ca90d4e688
JCH <ckdgusjang@naver.com>
2023-11-21 12:36:30 +0900

    feat : 채팅 메시지 전송 1차 완료 #3

--- BE/src/main/java/com/verda/BE/chat/dto/responseDto/RecieveMessageResponseDTO.java
+++ BE/src/main/java/com/verda/BE/chat/dto/responseDto/RecieveMessageResponseDTO.java
@@ -3,8 +3,13 @@ package com.verda.BE.chat.dto.responseDto;
 import java.time.LocalDateTime;
 
 public class RecieveMessageResponseDTO {
-    private long roomId;
     private String content;
     private String sender;
     private LocalDateTime sendTime;
+
+    public RecieveMessageResponseDTO(String content, String sender, LocalDateTime sendTime){
+        this.content=content;
+        this.sender=sender;
+        this.sendTime=sendTime;
+    }
 }
