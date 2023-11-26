34a19e7ddc02ce91d65584fc7bfc9bd9ac16b682
JCH <ckdgusjang@naver.com>
2023-11-26 20:01:23 +0900

    feat : redis 메시지브로커 설정 #3

--- BE/src/main/java/com/verda/BE/chat/controller/MessageController.java
+++ BE/src/main/java/com/verda/BE/chat/controller/MessageController.java
@@ -6,6 +6,7 @@ import com.verda.BE.chat.dto.responseDto.RecieveMessageResponseDTO;
 import com.verda.BE.chat.service.ChatService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
@@ -19,16 +20,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class MessageController {
     private final ChatService chatService;
     private final SimpMessagingTemplate template;
-    @GetMapping("/roomdetail/{roomId}")
-    public String enterRoom(@PathVariable long roomId, Model model){
-        model.addAttribute("roomId", roomId);
-        return "room";
-    }
+//    private final RedisTemplate<String, Object> redisTemplate;
 
     @Operation(summary = "채팅방 입장시 이전 채팅목록 조회", description = "채팅방 입장시 이전 채팅들을 불러옴")
     @GetMapping("/api/chat/{roomId}")
@@ -42,9 +39,16 @@ public class MessageController {
         template.convertAndSend("/sub/chat/room/"+chatMessageRequestDTO.getRoomId(),chatMessageRequestDTO.getContent());
     }
 
-    @MessageMapping("/api/send/messages")
+        @MessageMapping("/api/send/messages")
     public void message(@RequestBody ChatMessageRequestDTO chatMessageRequestDTO){
         RecieveMessageResponseDTO recieveMessage = chatService.sendMessage(chatMessageRequestDTO);
         template.convertAndSend("/sub/chat/room/" + chatMessageRequestDTO.getRoomId(), chatMessageRequestDTO.getContent());
     }
+    
+//    redis를 메시지 브로커로 하는 채팅
+//    @MessageMapping("/api/chat")
+//    public void message(@RequestBody ChatMessageRequestDTO chatMessageRequestDTO) {
+//        RecieveMessageResponseDTO recieveMessage = chatService.sendMessage(chatMessageRequestDTO);
+//        redisTemplate.convertAndSend("/sub/api/chat",chatMessageRequestDTO);
+//    }
 }
---
+++ BE/src/main/java/com/verda/BE/chat/redis/RedisSubscriber.java
@@ -0,0 +1,32 @@
+//package com.verda.BE.chat.redis;
+//
+//import com.fasterxml.jackson.core.JsonProcessingException;
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import com.verda.BE.chat.dto.requestDto.ChatMessageRequestDTO;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.data.redis.connection.Message;
+//import org.springframework.data.redis.connection.MessageListener;
+//import org.springframework.data.redis.core.RedisTemplate;
+//import org.springframework.messaging.simp.SimpMessageSendingOperations;
+//import org.springframework.stereotype.Service;
+//
+//@RequiredArgsConstructor
+//@Service
+//public class RedisSubscriber implements MessageListener {
+//
+//    private final RedisTemplate<String, Object> redisTemplate;
+//    private final ObjectMapper objectMapper;
+//    private final SimpMessageSendingOperations sendingOperations;
+//
+//    @Override
+//    public void onMessage(Message message, byte[] pattern) {
+//        try {
+//            String publishMessage = redisTemplate.getStringSerializer().deserialize(message.getBody());
+//            ChatMessageRequestDTO chatrequest = objectMapper.readValue(publishMessage, ChatMessageRequestDTO.class);
+//
+//            sendingOperations.convertAndSend("/sub/api/chat/"+chatrequest.getRoomId(), chatrequest.getContent());
+//        } catch (JsonProcessingException e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
+//}
