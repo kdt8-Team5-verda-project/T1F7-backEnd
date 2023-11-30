496be5105c99e53ecd90dee1bc0641ada158a166
kimjonghyeon6084 <ssoert123@naver.com>
2023-11-30 13:07:43 +0900

    feat: 에러코드 수정 #1

--- BE/src/main/java/com/verda/BE/common/ErrorCode.java
+++ BE/src/main/java/com/verda/BE/common/ErrorCode.java
@@ -16,8 +16,8 @@ public enum ErrorCode {
     NOT_FOUND_CHAT(404, "조회되는 채팅방이없습니다."),
 
     /* 회원 관련 */
-    MEMBER_NOT_FOUND(404, "해당 회원을 찾을 수 없습니다."),
-    EMAIL_NOT_FOUND(404, "해당 이메일을 찾을 수 없습니다.")
+    NOT_FOUND_MEMBER(404, "해당 회원을 찾을 수 없습니다."),
+    NOT_FOUND_EMAIL(404, "해당 이메일을 찾을 수 없습니다.")
     ;
 
     private Integer status;
